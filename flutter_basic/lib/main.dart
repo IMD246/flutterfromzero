ee1b3fd308cc478c16facb842a7f09419f9d448d
IMD246 <tduy24699@gmail.com>
2021-12-01 22:04:34 +0700

    setstate, floating,UI basic

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -3,15 +3,143 @@ import "package:flutter/cupertino.dart"
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(CounterApp());
+  runApp(MaterialApp(
+    home: CounterApp(),
+  ));
 }
 
-class CounterApp extends StatelessWidget {
+class CounterApp extends StatefulWidget {
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int ninjaLevel = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        child: Text('Xin chào'),
+    return Scaffold(
+      backgroundColor: Colors.grey[400],
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: const Text('Demo'),
+        elevation: 0.00,
+        centerTitle: true,
+        leading: Builder(
+            builder: (context) => IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                )),
+        actions: [
+          Builder(
+              builder: (context) => IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {
+                      Scaffold.of(context).openEndDrawer();
+                    },
+                  )),
+        ],
+      ),
+      drawer: Drawer(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Press')),
+      ),
+      endDrawer: Drawer(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Press')),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 20.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/h1.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[400],
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Duy",
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Current Ninja Level",
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "$ninjaLevel",
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.black,
+                ),
+                Text(
+                  'tduy@gmail.com',
+                  style: TextStyle(
+                    color: Colors.green[300],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[850],
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 50,
+        ),
       ),
     );
   }
