d4c738bf084df2f79b0e3bdaacf511d2f83beae8
IMD246 <tduy24699@gmail.com>
2021-12-13 00:24:00 +0700

    Animation Controllers

--- ninja_trips/lib/shared/heart.dart
+++ ninja_trips/lib/shared/heart.dart
@@ -5,16 +5,53 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _colorAnimation;
+  bool isFav = false;
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _colorAnimation = ColorTween(
+      begin: Colors.grey[400],
+      end: Colors.red,
+    ).animate(_controller);
+
+    _controller.addListener(() {});
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isFav = true;
+        });
+      } else {
+        setState(() {
+          isFav = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, _) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            isFav ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
   }
 }
